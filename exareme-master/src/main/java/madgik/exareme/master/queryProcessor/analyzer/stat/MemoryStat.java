e98624a1c0ca9fd0a20b94a7ddfc02b6440d326d
dimitris <grad0903@di.uoa.gr>
2019-04-02 14:52:09 +0300

    bug in threads

--- exareme-master/src/main/java/madgik/exareme/master/queryProcessor/analyzer/stat/MemoryStat.java
+++ exareme-master/src/main/java/madgik/exareme/master/queryProcessor/analyzer/stat/MemoryStat.java
@@ -15,6 +15,8 @@ import madgik.exareme.master.queryProcessor.estimator.histogram.Bucket;
 import madgik.exareme.master.queryProcessor.estimator.histogram.Histogram;
 import org.apache.log4j.Logger;
 
+import static org.junit.Assert.assertTrue;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -23,8 +25,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -171,58 +176,63 @@ public class MemoryStat {
 
 	}
 
-	private JoinCardinalities computeJoins(int typeProperty) throws SQLException {
+	private JoinCardinalities computeJoins(int typeProperty) throws Exception {
 		JoinCardinalities cards = new JoinCardinalities();
 		sizes.sort(new SizeComparator());
 		// Statement stmt1 = con.createStatement();
-		for (int i = 0; i < sizes.size(); i++) {
+		ExecutorService exService = Executors.newFixedThreadPool(DecomposerUtils.CARDINALITY_THREADS);
+		try {
 
-			try {
-				final ExecutorService exService = Executors.newFixedThreadPool(DecomposerUtils.CARDINALITY_THREADS);
-				exService.submit(new CardinalityEstimator(i, typeProperty, cards), "done");
-				exService.shutdown();
-				exService.awaitTermination(3600, TimeUnit.SECONDS);
-				
-			} catch (InterruptedException e) {
-				System.err.println("Could not estimate cardinality");
-				e.printStackTrace();
+			ExecutorCompletionService<Boolean> ecs = new ExecutorCompletionService<Boolean>(exService);
+			for (int i = 0; i < sizes.size(); i++) {
+				ecs.submit(new CardinalityEstimator(i, typeProperty, cards));
+
+			}
+			for (int i = 0; i < sizes.size(); i++) {
+				Future<Boolean> result = ecs.take();
+				assertTrue(result.get());
 			}
 
-			/*
-			 * int tblName = 0; TableSize ts = sizes.get(i); if (ts.getTable() ==
-			 * typeProperty) { continue; } tblName = ts.getTable();
-			 * 
-			 * for (int j = i + 1; j < sizes.size(); j++) { int tblName2 = 0; TableSize ts2
-			 * = sizes.get(j); if (ts2.getTable() == typeProperty) { continue; } //
-			 * if(ts.getTable()<0 && ts2.getTable()<0){ // continue; // } tblName2 =
-			 * ts2.getTable();
-			 * 
-			 * ResultSet mode2 = stmt1.
-			 * executeQuery("select result from stat2 where mode=2 and option1=0 and option2="
-			 * + tblName + " and option3=" + tblName2);
-			 * 
-			 * int countSS = mode2.getInt(1); int countSO = 0; int countOS = 0; int countOO
-			 * = 0; mode2.close(); if (ts2.getTable() > -1) { mode2 = stmt1.
-			 * executeQuery("select result from stat2 where mode=2 and option1=1 and option2="
-			 * + tblName + " and option3=" + tblName2); countSO = mode2.getInt(1);
-			 * mode2.close(); } if (ts.getTable() != typeProperty) { mode2 = stmt1.
-			 * executeQuery("select result from stat2 where mode=2 and option1=2 and option2="
-			 * + tblName + " and option3=" + tblName2); countOS = mode2.getInt(1);
-			 * mode2.close(); if (ts2.getTable() > -1) { mode2 = stmt1.
-			 * executeQuery("select result from stat2 where mode=2 and option1=3 and option2="
-			 * + tblName + " and option3=" + tblName2); countOO = mode2.getInt(1);
-			 * mode2.close();
-			 * 
-			 * } }
-			 * 
-			 * if (ts.getTable() < ts2.getTable()) { cards.add(ts.getTable(),
-			 * ts2.getTable(), countSS, countSO, countOS, countOO); } else {
-			 * cards.add(ts2.getTable(), ts.getTable(), countSS, countOS, countSO, countOO);
-			 * }
-			 * 
-			 * }
-			 */
+		} finally {
+			exService.shutdown();
+			exService.awaitTermination(3600, TimeUnit.SECONDS);
 		}
+
+		/*
+		 * int tblName = 0; TableSize ts = sizes.get(i); if (ts.getTable() ==
+		 * typeProperty) { continue; } tblName = ts.getTable();
+		 * 
+		 * for (int j = i + 1; j < sizes.size(); j++) { int tblName2 = 0; TableSize ts2
+		 * = sizes.get(j); if (ts2.getTable() == typeProperty) { continue; } //
+		 * if(ts.getTable()<0 && ts2.getTable()<0){ // continue; // } tblName2 =
+		 * ts2.getTable();
+		 * 
+		 * ResultSet mode2 = stmt1.
+		 * executeQuery("select result from stat2 where mode=2 and option1=0 and option2="
+		 * + tblName + " and option3=" + tblName2);
+		 * 
+		 * int countSS = mode2.getInt(1); int countSO = 0; int countOS = 0; int countOO
+		 * = 0; mode2.close(); if (ts2.getTable() > -1) { mode2 = stmt1.
+		 * executeQuery("select result from stat2 where mode=2 and option1=1 and option2="
+		 * + tblName + " and option3=" + tblName2); countSO = mode2.getInt(1);
+		 * mode2.close(); } if (ts.getTable() != typeProperty) { mode2 = stmt1.
+		 * executeQuery("select result from stat2 where mode=2 and option1=2 and option2="
+		 * + tblName + " and option3=" + tblName2); countOS = mode2.getInt(1);
+		 * mode2.close(); if (ts2.getTable() > -1) { mode2 = stmt1.
+		 * executeQuery("select result from stat2 where mode=2 and option1=3 and option2="
+		 * + tblName + " and option3=" + tblName2); countOO = mode2.getInt(1);
+		 * mode2.close();
+		 * 
+		 * } }
+		 * 
+		 * if (ts.getTable() < ts2.getTable()) { cards.add(ts.getTable(),
+		 * ts2.getTable(), countSS, countSO, countOS, countOO); } else {
+		 * cards.add(ts2.getTable(), ts.getTable(), countSS, countOS, countSO, countOO);
+		 * }
+		 * 
+		 * }
+		 */
+		// }
 		return cards;
 	}
 
@@ -297,7 +307,7 @@ public class MemoryStat {
 		}
 	}
 
-	private class CardinalityEstimator implements Runnable {
+	private class CardinalityEstimator implements Callable<Boolean> {
 
 		int propIndex;
 		int typeProperty;
@@ -310,11 +320,11 @@ public class MemoryStat {
 		}
 
 		@Override
-		public void run() {
+		public Boolean call() {
 			int tblName = 0;
 			TableSize ts = sizes.get(propIndex);
 			if (ts.getTable() == typeProperty) {
-				return;
+				return true;
 			}
 			tblName = ts.getTable();
 			Statement stmt1;
@@ -380,9 +390,11 @@ public class MemoryStat {
 					}
 				}
 				stmt1.close();
+				return true;
 			} catch (SQLException e) {
 				System.err.println("Could not estimate cardinality");
 				e.printStackTrace();
+				return false;
 			}
 
 		}
